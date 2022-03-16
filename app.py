7f3f97b3a29a9ea8a8d33206b518d4ef00056941
ritik dutta <ritikduttagd@gmail.com>
2022-03-16 16:40:24 +0530

    updated app

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import os
-import yaml
-import joblib
 import numpy as np
 from prediction_service import prediction
 webapp_root = "webapp"
@@ -9,15 +7,16 @@ webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data_req = dict(request.form)
-                response = prediction.form_response(data_req)
+                dict_req = dict(request.form)
+                response = prediction.form_response(dict_req)
                 return render_template("index.html", response=response)
 
             elif request.json:
@@ -26,8 +25,10 @@ def index():
 
         except Exception as e:
             print(e)
-            # error = {"error": "Something went wrong!! Try again"}
-            return render_template("404.html", error=e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+
+            return render_template("404.html", error=error)
     else:
         return render_template("index.html")
 
