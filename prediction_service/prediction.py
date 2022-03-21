c82f57eaf8f160bc3a709c911176256a9d613433
ritik dutta <ritikduttagd@gmail.com>
2022-03-21 13:26:36 +0530

    creating test

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -71,9 +71,14 @@ def api_response(dict_request):
     try:
         if validate_input(dict_request):
             data = np.array([list(dict_request.values())])
-            response = predict()
+            response = predict(data)
             response = {"response": response}
             return response
-    except Exception as e:
-        response = {"the_expected_range": get_schema(), "response":str(e)}
+            
+    except NotInRange as e:
+        response = {"the_exected_range": get_schema(), "response": str(e) }
+        return response
+
+    except NotInCols as e:
+        response = {"the_exected_cols": get_schema().keys(), "response": str(e) }
         return response
